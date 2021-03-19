0c770c8ad8ca05f313644edc7569e9effeffe5c1
Félix Saparelli <felix@passcod.name>
2021-03-19 20:35:49 +1300

    Add all the waffle

---
+++ README.md
@@ -0,0 +1,210 @@
+[![Crate release version](https://flat.badgen.net/crates/v/passcod-noodle)](https://crates.io/crates/passcod-noodle)
+[![Crate license: Apache 2.0 or MIT](https://flat.badgen.net/badge/license/Apache%202.0%20or%20MIT)][copyright]
+![MSRV: latest stable](https://flat.badgen.net/badge/MSRV/latest%20stable/orange)
+[![Uses Caretaker Maintainership](https://flat.badgen.net/badge/Caretaker/Maintainership%20👥%20/purple)][caretaker]
+
+# Noodle
+
+_AKA "someone stop Félix from naming things, please, this is terrible."_
+
+- Go directly to the [usage summary](#obtain).
+- [Dual-licensed][copyright] with Apache 2.0 and MIT.
+- Uses [Caretaker Maintainership][caretaker].
+
+[caretaker]: ./CARETAKERS.md
+[copyright]: ./COPYRIGHT
+
+## Introduction
+
+Noodle as in pool noodle, because it floats and IPs and... geddit?
+
+The idea is based on [MetalLB](https://metallb.universe.tf/concepts/layer2/).
+
+Basically, we abuse ARP. (And later, NDP for IPv6.)
+
+Instead of ARP being request-reply "who has 1.2.3.4?" "I do", we send
+"I have 1.2.3.4" and we add 1.2.3.4 to the network interface we do that
+from, so the network stack handles traffic sent to it properly.
+
+Noodle is the little daemon that does the ARP advertisement like that.
+
+But Noodle also listens for ARP on the interface. If it sees ARP for the
+IP it's claiming for the interface, but with a different MAC address, it
+stops and exits!
+
+When it stops, if it's being managed by an active orchestrator or supervisor,
+it will get restarted. And if it's not, but for some reason wasn't shut down
+when the orchestrator or supervisor went down, it wont come back up.
+
+In any case, an active Noodle will keep spraying its ARP over the subnet
+every N seconds, so well behaved devices like routers and VMs will keep
+their ARP tables updated with the MAC of whatever device Noodle is on.
+
+Essentially it's like MetalLB but we use the network itself as an additional
+consensus layer, and the main consensus is via whatever orchestrator you use.
+You can also disable the ARP watcher and/or only send a number of ARPs and then
+quit, so you can build the orchestration a little more granularly yourself if
+you want. It's up to you!
+
+BTW this was written for use with Nomad but doesn't in any way depend on Nomad.
+
+## Good times
+
+### In a graceful move situation
+
+Orchestrator will boot Noodle on the other Node, and then either the
+orchestrator or the new ARP will kill the old Noodle. ARP tables will be
+updated subnet-wide and most importantly at the router, and the IP will
+essentially have "floated" over to the new node in short order.
+
+The orchestrator, when configured properly, can also keep the old service
+running for a bit before killing it even while starting the new one, so traffic
+will move over as smoothly as it can (long connections may still get broken).
+
+### In a failover situation
+
+The old node will be dead. Traffic goes nowhere. Devices on the subnet
+and the router still have the old MAC in the ARP tables.
+
+Orchestrator notices a node is down, and reschedules its workload on the alive
+nodes. Noodle starts, blathers unsolicited ARP over the network, tables get
+updated, and traffic starts flowing again.
+
+### In a split brain situation
+
+Orchestrator worker node gets separated from the orchestrator's servers, or
+talks to only one server and _it_ is separated from the others. In either case,
+that node's orchestrator worker agent will:
+
+- appear dead to the rest of the cluster, which will reschedule work as
+  in the above, and
+- notice that it's dead or doesn't have consensus, and _ideally_ will
+  kill itself.
+
+Proceeds mostly the same as failover, but may proceed as crash:
+
+### In an orchestrator crash situation
+
+Orchestrator crashes, leaving the underlying containers or applications
+running. Rest of the cluster notices the node is dead and reschedules. The new
+Noodle yells its ARP out, the old Noodle notices ARP that is not coming from
+itself, and exits. Traffic moves over to the new node.
+
+## Bad times
+
+### Partial split brain where the old node still thinks it's meant to be up
+
+Old orchestrator appears dead to the rest of the cluster, which reschedules.
+
+Old Noodle notices ARP, and kills itself.
+
+Old orchestrator restarts its Noodle.
+
+New Noodle notices ARP, and kills itself.
+
+New orchestrator restarts its Noodle.
+
+Round and round it goes, traffic is mightily confused, routers might
+take a hit.
+
+### In a Noodle crash situation
+
+Noodle crashes in the middle of its loops and doesn't clean up the IP on
+its interface. Linux responds to ARP requests with solicited "it's me!"
+while other Noodle is screaming out "it's me it's me it's me" unsolicited.
+
+Traffic and routers get confused.
+
+_This is always a bug and should be reported._
+
+### Operator accidentally scales the Noodle service for one floating IP to 2 or more instances
+
+Proceeds like partial split brain except both nodes are technically legitimate.
+
+## Alternatives
+
+- Native Cloud Provider Floating IP: absolutely use that if available.
+
+- Tunnels: requires router access to set up and tunnel endpoint at the
+  service, can be connected to twice concurrently and then what. Adds a
+  moving piece. But is much quieter on the network, cleaner
+  traffic-wise, maybe clearer to netops.
+
+- BGP: requires router access to set up, and complicated daemon on every
+  node, plus management of said daemons' config. Does offer true IP-level
+  load-balancing instead of just directing traffic to one host. BGP can
+  be a pain and also may interfere with netops.
+
+- DNS: externalises the problem, very slow to update even with TTL=0 because of
+  client behaviour. Some traffic just never dies.
+
+- Single VM acts as load-balancer and is never rebooted: not really an
+  option, this is what we're trying to get away from.
+
+- Transferring the entire NIC at the VM hypervisor level between machines
+  instead of doing the ARP dance: certainly an option, relies on having
+  hypervisor API access, needs some kind of centralised control to avoid
+  split brain and conflicts. Implies downtime during the switching.
+
+- Just changing the hypervisor MAC of the interfaces: same as previous,
+  plus adds even more downtime as MACs can't conflict across VMs and
+  it needs to find a free "transfer" mac or shut the old NIC down.
+
+- Changing the ARP table of the router directly with the router's
+  administrative API: doesn't yell so much into the void, but requires access
+  to router, and some kind of synchronisation to avoid conflicting commands.
+
+- VRRP, Tree Spanning, OSPF, bonding, etc: cool tech, may work, requires
+  netops. May not handle intra-subnet traffic, only via router.
+
+## Obtain
+
+Only works on Linux.
+
+### From binary release
+
+The [release tab on GitHub](https://github.com/passcod/noodle/releases).
+
+### With cargo binstall
+
+```
+cargo binstall passcod-noodle
+```
+
+### From source
+
+```
+cargo install passcod-noodle
+```
+
+You can also compile from the repo as usual.
+
+## Use
+
+Minimal command:
+
+```
+noodle --ip 10.9.8.7/24 --interface ens123
+```
+
+Mandatory options:
+
+- `--ip=IP/SUBNET`: the floating IP to claim.
+- `--interface=NAME`: which interface to blast ARP/NDP on.
+
+Other options:
+
+- `--mac=ADDRESS`: override the MAC address (default=read from interface)
+- `--log=LEVEL`: specify the log level (default=info). All logs are JSON.
+- `--interval=DURATION` in seconds (default=10): how often to blast.
+- `--delay=DURATION` in seconds (default=0): delay the first blast.
+- `--jitter=DURATION` in seconds (default=2): add jitter to each delay and
+  interval that's a random value between zero and this value.
+- `--watch=BEHAVIOUR`: control the advertisement watcher:
+  * `fail` (default): exit with status 17 if we see an advertisement for this
+    IP by another MAC address
+  * `quit`: exit with status 0 instead
+  * `log`: don't exit, only log it
+  * `no`: don't watch
+- `--count=N` (default=0/disabled): only blast this many times.
+- `--once`: shorthand for `--count=1 --delay=0 --jitter=0 --watch=no`.
--- src/main.rs
+++ src/main.rs
@@ -1,162 +1,210 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 use std::{
-    convert::TryFrom,
-    net::{Ipv4Addr, Ipv6Addr},
-    result::Result as StdResult,
-    str::FromStr,
-    time::Duration,
+	convert::TryFrom,
+	net::{Ipv4Addr, Ipv6Addr},
+	result::Result as StdResult,
+	str::FromStr,
+	time::Duration,
 };
 
 use argh::FromArgs;
-use async_std::{channel, task::{spawn, spawn_blocking}};
+use async_std::{
+	channel,
+	task::{spawn, spawn_blocking},
+};
 use color_eyre::eyre::{eyre, Result};
 use futures::{future::TryFutureExt, stream::TryStreamExt, try_join};
-use pnet::{datalink::{Channel, ChannelType, Config, interfaces, channel as datachannel}, util::MacAddr, ipnetwork::IpNetwork, packet::{arp::{ArpOperations, ArpPacket}, ethernet::EtherTypes}};
+use pnet::{
+	datalink::{channel as datachannel, interfaces, Channel, ChannelType, Config},
+	ipnetwork::IpNetwork,
+	packet::{
+		arp::{ArpOperations, ArpPacket},
+		ethernet::EtherTypes,
+	},
+	util::MacAddr,
+};
 use rtnetlink::{packet::rtnl::address::nlas::Nla, AddressHandle};
 
 /// Claim an IP for an interface via ARP/NDP. If we hear about someone else claiming the IP, stop.
 #[derive(Debug, FromArgs)]
 struct Args {
-    /// interface to claim on
-    #[argh(option)]
-    interface: String,
+	/// interface to claim on
+	#[argh(option)]
+	interface: String,
 
-    /// ip (optionally with subnet, defaults to /32) to claim
-    #[argh(option)]
-    ip: IpNetwork,
+	/// ip (optionally with subnet, defaults to /32) to claim
+	#[argh(option)]
+	ip: IpNetwork,
 
-    /// interval to send claims
-    #[argh(option, from_str_fn(str_to_secs), default = "Duration::from_secs(60)")]
-    interval: Duration,
+	/// mac address override
+	#[argh(option)]
+	mac: Option<MacAddr>,
 
-    /// mac address override
-    #[argh(option)]
-    mac: Option<MacAddr>,
+	/// log level (default=info)
+	#[argh(option)]
+	log: String,
+
+	/// interval in seconds to send claims (default=10)
+	#[argh(option, from_str_fn(str_to_secs), default = "Duration::from_secs(10)")]
+	interval: Duration,
+
+	/// delay in seconds before sending first claim (default=0/disabled)
+	#[argh(option, from_str_fn(str_to_secs), default = "Duration::from_secs(0)")]
+	delay: Duration,
+
+	/// add some random [0 - value in seconds] jitter to delay and interval (default=2)
+	#[argh(option, from_str_fn(str_to_secs), default = "Duration::from_secs(2)")]
+	jitter: Duration,
+
+	/// blast advertisements this many times then stop (default=0/disabled)
+	#[argh(option)]
+	count: usize,
+
+	/// control the advertisement watcher (default=fail)
+	///
+	/// - `fail` (default): exit with status 17 if we see an advertisement for this IP by another MAC address
+	/// - `quit`:
+	/// - `log`:
+	/// - `no`:
+	#[argh(option)]
+	watch: String,
+
+	/// shorthand for `--count=1 --watch=no`
+	#[argh(option)]
+	once: bool,
 }
 
 fn str_to_secs(s: &str) -> StdResult<Duration, String> {
-    u64::from_str(s)
-        .map_err(|e| e.to_string())
-        .map(Duration::from_secs)
+	u64::from_str(s)
+		.map_err(|e| e.to_string())
+		.map(Duration::from_secs)
 }
 
 #[async_std::main]
 async fn main() -> Result<()> {
-    color_eyre::install()?;
-    let args: Args = argh::from_env();
+	color_eyre::install()?;
+	let args: Args = argh::from_env();
 
-    let ethertype = match args.ip {
-        IpNetwork::V4(_) => EtherTypes::Arp,
-        IpNetwork::V6(_) => {
-            todo!("ipv6/ndp support");
-            EtherTypes::Ipv6
-        }
-    };
+	let ethertype = match args.ip {
+		IpNetwork::V4(_) => EtherTypes::Arp,
+		IpNetwork::V6(_) => {
+			todo!("ipv6/ndp support");
+			EtherTypes::Ipv6
+		}
+	};
 
-    dbg!(&args);
+	dbg!(&args);
 
-    let interface = interfaces()
-        .into_iter()
-        .find(|i| i.name == args.interface)
-        .ok_or(eyre!("interface does not exist"))?;
-    if interface.mac.is_none() {
-        return Err(eyre!("interface does not have a mac address"));
-    }
-    if interface.is_loopback() {
-        return Err(eyre!("cannot use loopback interface"));
-    }
-    if interface.is_point_to_point() {
-        return Err(eyre!("cannot use point-to-point interface"));
-    }
-    if !interface.is_up() {
-        return Err(eyre!("interface must be up"));
-    }
+	let interface = interfaces()
+		.into_iter()
+		.find(|i| i.name == args.interface)
+		.ok_or(eyre!("interface does not exist"))?;
+	if interface.mac.is_none() {
+		return Err(eyre!("interface does not have a mac address"));
+	}
+	if interface.is_loopback() {
+		return Err(eyre!("cannot use loopback interface"));
+	}
+	if interface.is_point_to_point() {
+		return Err(eyre!("cannot use point-to-point interface"));
+	}
+	if !interface.is_up() {
+		return Err(eyre!("interface must be up"));
+	}
 
-    let (mut tx, mut rx) = match datachannel(&interface, Config {
-        channel_type: ChannelType::Layer3(ethertype.0),
-        promiscuous: true,
-        ..Default::default()
-    })? {
-        Channel::Ethernet(tx, rx) => (tx, rx),
-        _ => unimplemented!("internal: unhandled datachannel type"),
-    };
+	let (mut tx, mut rx) = match datachannel(
+		&interface,
+		Config {
+			channel_type: ChannelType::Layer3(ethertype.0),
+			promiscuous: true,
+			..Default::default()
+		},
+	)? {
+		Channel::Ethernet(tx, rx) => (tx, rx),
+		_ => unimplemented!("internal: unhandled datachannel type"),
+	};
 
-    let (oconnor, terminator) = channel::bounded(1);
-    ctrlc::set_handler(move || {
-        oconnor.try_send(()).expect("failed to exit, so exiting harder (unclean)");
-    })?;
+	let (oconnor, terminator) = channel::bounded(1);
+	ctrlc::set_handler(move || {
+		oconnor
+			.try_send(())
+			.expect("failed to exit, so exiting harder (unclean)");
+	})?;
 
-    // ctrl-c / sig{term,int} handler
+	// ctrl-c / sig{term,int} handler
 
-    // => spawn off: listen for arp on the interface
-    // - if there's a arp for our ip but not our mac, stop noodle
-    let listener = spawn_blocking(move || -> Result<()> { loop {
-        match rx.next()? {
-            pkt => {
-                match ethertype {
-                    EtherTypes::Arp => {
-                        let arp = ArpPacket::new(pkt).ok_or(eyre!("arp packet buffer too small"))?;
-                        let op = match arp.get_operation() {
-                            ArpOperations::Reply => "reply",
-                            ArpOperations::Request => "request",
-                            _ => "unk"
-                        };
-                        eprintln!("arp {}: {:?}", op, arp);
-                    }
-                    EtherTypes::Ipv6 => todo!("v6 support"),
-                    _ => unreachable!()
-                }
-            },
-        }
-    } });
+	// => spawn off: listen for arp on the interface
+	// - if there's a arp for our ip but not our mac, stop noodle
+	let listener = spawn_blocking(move || -> Result<()> {
+		loop {
+			match rx.next()? {
+				pkt => match ethertype {
+					EtherTypes::Arp => {
+						let arp =
+							ArpPacket::new(pkt).ok_or(eyre!("arp packet buffer too small"))?;
+						let op = match arp.get_operation() {
+							ArpOperations::Reply => "reply",
+							ArpOperations::Request => "request",
+							_ => "unk",
+						};
+						eprintln!("arp {}: {:?}", op, arp);
+					}
+					EtherTypes::Ipv6 => todo!("v6 support"),
+					_ => unreachable!(),
+				},
+			}
+		}
+	});
 
-    let (nlconn, nl, _) = rtnetlink::new_connection()?;
-    spawn(nlconn);
+	let (nlconn, nl, _) = rtnetlink::new_connection()?;
+	spawn(nlconn);
 
-    let nlah = AddressHandle::new(nl);
+	let nlah = AddressHandle::new(nl);
 
-    eprintln!("adding ip to interface");
-    nlah.add(interface.index, args.ip.ip(), args.ip.prefix())
-        .execute()
-        .await?;
+	eprintln!("adding ip to interface");
+	nlah.add(interface.index, args.ip.ip(), args.ip.prefix())
+		.execute()
+		.await?;
 
-    // loop: every interval, send arp. start now
+	// loop: every interval, send arp. start now
 
-    if let Err(err) = try_join!(
-        terminator.recv().map_err(|e| e.into()).and_then(|_| async {
-            Err(eyre!("Ctrl-C received, quitting gracefully")) as Result<()>
-        }),
-        listener
-    ) {
-        eprintln!("{:?}", err);
-    }
+	if let Err(err) = try_join!(
+		terminator.recv().map_err(|e| e.into()).and_then(|_| async {
+			Err(eyre!("Ctrl-C received, quitting gracefully")) as Result<()>
+		}),
+		listener
+	) {
+		eprintln!("{:?}", err);
+	}
 
-    eprintln!("removing ip from interface");
-    let mut addrlist = nlah.get().execute();
-    while let Some(addr) = addrlist.try_next().await? {
-        if addr.header.index != interface.index {
-            continue;
-        }
+	eprintln!("removing ip from interface");
+	let mut addrlist = nlah.get().execute();
+	while let Some(addr) = addrlist.try_next().await? {
+		if addr.header.index != interface.index {
+			continue;
+		}
 
-        let addrbytes = match addr.nlas.iter().find(|n| matches!(n, Nla::Address(_))) {
-            Some(Nla::Address(a)) => a,
-            _ => continue,
-        };
+		let addrbytes = match addr.nlas.iter().find(|n| matches!(n, Nla::Address(_))) {
+			Some(Nla::Address(a)) => a,
+			_ => continue,
+		};
 
-        match args.ip {
-            IpNetwork::V4(ip4) => {
-                match <[u8; 4]>::try_from(addrbytes.clone()).map(Ipv4Addr::from) {
-                    Ok(ar) if ar == ip4.ip() => nlah.del(addr).execute().await?,
-                    _ => continue,
-                };
-            }
-            IpNetwork::V6(ip6) => {
-                match <[u8; 16]>::try_from(addrbytes.clone()).map(Ipv6Addr::from) {
-                    Ok(ar) if ar == ip6.ip() => nlah.del(addr).execute().await?,
-                    _ => continue,
-                };
-            }
-        };
-    }
+		match args.ip {
+			IpNetwork::V4(ip4) => {
+				match <[u8; 4]>::try_from(addrbytes.clone()).map(Ipv4Addr::from) {
+					Ok(ar) if ar == ip4.ip() => nlah.del(addr).execute().await?,
+					_ => continue,
+				};
+			}
+			IpNetwork::V6(ip6) => {
+				match <[u8; 16]>::try_from(addrbytes.clone()).map(Ipv6Addr::from) {
+					Ok(ar) if ar == ip6.ip() => nlah.del(addr).execute().await?,
+					_ => continue,
+				};
+			}
+		};
+	}
 
-    Ok(())
+	Ok(())
 }
